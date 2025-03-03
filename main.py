1e0e6321bb333dc5c04cc7fb8d1c8cfa994c515c
shia <nahsor2242195@gmail.com>
2025-03-03 10:32:06 +0900

    kadai_014

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_tax(price: int, tax: int):
-    return price + price * (tax / 100)
+price1 = 100
+price2 = 200
+tax = 1.1
 
-tax_add_price = add_tax(110, 10)
-print(tax_add_price)
+def total():
+    return price1 + price2
+
+print (total() * tax)
