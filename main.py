3e06e9ed4f6302da97cd0865dd5e582d48194489
shia <nahsor2242195@gmail.com>
2025-03-05 16:55:03 +0900

    kadai_017

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ class Human:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-    def printinfo(self):
-        print(self.name + ' ' + str(self.age))
 
-human = Human("test", 30)
-human.printinfo()
+    def check_adult(self):
+        if self.age >= 20:
+            print("adult")
+        else:
+            print("child")
+
+humans = [Human("human1", 21), Human("human2", 19)]
+human3 = Human("human3", 20)
+humans.append(human3)
+for human in humans:
+    human.check_adult()
